9926868861d3a878c3cbf6663df64f30d331e4f8
decbbbor <wjdqhfk200@gmail.com>
2024-02-28 16:30:27 +0900

    intent

---
+++ app/src/main/java/com/example/smilebook/join.java
@@ -0,0 +1,14 @@
+package com.example.smilebook;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class join extends AppCompatActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.join);
+    }
+
+}
---
+++ app/src/main/java/com/example/smilebook/login.java
@@ -0,0 +1,17 @@
+package com.example.smilebook;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.smilebook.R;
+
+public class login extends AppCompatActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        임시로 activity_main 연결
+//        추후 나영이 코드 연결하면 수정하기
+        setContentView(R.layout.test);
+    }
+}
