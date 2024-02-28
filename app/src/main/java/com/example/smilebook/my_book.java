fa937a0df53cd357f10282da2766829123ffc143
lk051111 <lk051111@naver.com>
2024-02-28 18:35:06 +0900

    test2

---
+++ app/src/main/java/com/example/smilebook/my_book.java
@@ -0,0 +1,25 @@
+package com.example.smilebook;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class my_book extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_book);
+
+        Button location = (Button) findViewById(R.id.location);
+        location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), location.getClass());
+                startActivity(intent);
+            }
+        });
+    }
+}
