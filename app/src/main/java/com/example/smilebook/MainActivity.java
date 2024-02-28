3c9e4df8ec9e6c6e1f8fe1db06f26aa26e0193e8
decbbbor <wjdqhfk200@gmail.com>
2024-02-28 16:30:27 +0900

    intent

--- app/src/main/java/com/example/smilebook/MainActivity.java
+++ app/src/main/java/com/example/smilebook/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.smilebook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
+
+        Button loginbtn = (Button) findViewById(R.id.loginButton);
+        Button joinbtn = (Button) findViewById(R.id.joinButton);
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent  = new Intent(getApplicationContext(), login.class);
+                startActivity(intent);
+            }
+        });
+
+        joinbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), join.class);
+                startActivity(intent);
+            }
+        });
     }
 }
