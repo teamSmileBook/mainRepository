2a6c7f0e17a6c3f1a17bb75e6483ceb85526b22f
lk051111 <lk051111@naver.com>
2024-03-14 12:24:14 +0900

    3/14 나영커밋

---
+++ app/src/main/java/com/example/smilebook/book_list.java
@@ -0,0 +1,16 @@
+package com.example.smilebook;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class book_list extends AppCompatActivity {
+    public book_list() {
+    }
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(layout.book_list);
+    }
+}
