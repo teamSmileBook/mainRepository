5d13ae13d43c6755d613ee04b7fa9100badf7bc5
si so yeon <amy10041009@gmail.com>
2024-03-30 23:25:12 +0900

    new

--- app/src/main/java/com/example/smilebook/MainActivity.java
+++ app/src/main/java/com/example/smilebook/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
                 //login.class 였는데 화면 연결 꼬여서 main_b.class로 연결해둠
                 //로그인 버튼 누르면 바로 main_b 화면으로 연결 되는 상황!
                 //추후 로그인 기능 넣으면서 화면 흐름 바꾸려면 여기 수정하기
-                Intent intent  = new Intent(getApplicationContext(), main_b.class);
+                Intent intent  = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(intent);
             }
         });
