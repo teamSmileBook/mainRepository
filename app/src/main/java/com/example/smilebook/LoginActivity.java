78d5d009988428edf59a559dc7830112d2eb453c
si so yeon <amy10041009@gmail.com>
2024-04-26 15:34:03 +0900

    4/26 소연

--- app/src/main/java/com/example/smilebook/LoginActivity.java
+++ app/src/main/java/com/example/smilebook/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.smilebook;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -40,6 +41,11 @@ public class LoginActivity extends AppCompatActivity {
             String password = passwordEditText.getText().toString().trim();
 
             if (!username.isEmpty() && !password.isEmpty()) {
+                // SharedPreferences를 사용하여 username 값을 저장
+                SharedPreferences.Editor editor = getSharedPreferences("MyPrefs", MODE_PRIVATE).edit();
+                editor.putString("username", username);
+                editor.apply();
+
                 login(username, password);
             } else {
                 Toast.makeText(LoginActivity.this, "아이디와 비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show();
